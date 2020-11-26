42019ec46a89990f43e8c27da09f4875e2865653
NSQA <nguyensyquanganh@gmail.com>
2020-11-26 22:24:55 +0700

    add counter UI and two buttons to screen

--- src/containers/home/screens/Feed.js
+++ src/containers/home/screens/Feed.js
@@ -2,34 +2,43 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import FilledButton from "../../../components/FilledButton";
-import { MyColors } from "../../../styles/theme";
+import ViewBox from "../../../components/ViewBox";
+import TextView from "../../../components/TextView";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../../../redux/actions/counter";
 
 // create a component
-const Feed = ({ navigation }) => {
+const Feed = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch({
+      type: actions.INCREMENT_COUNTER,
+      payload: counter + 1,
+    });
+  };
+
+  const handleDecrement = () => {
+    dispatch({
+      type: actions.DECREMENT_COUNTER,
+      payload: counter - 1,
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 30,
-          fontFamily: "Quicksand",
-          color: "#fff",
-          textAlign: "center",
-        }}>
-        Almost before we knew it, we had left the ground.
-      </Text>
-    </View>
+    <ViewBox centered middle baseColor flex>
+      <TextView white size={20} margin={20}>
+        Counter: {counter}
+      </TextView>
+
+      <FilledButton width={150} title="+" onPress={handleIncrement} />
+      <FilledButton width={150} title="-" onPress={handleDecrement} />
+    </ViewBox>
   );
 };
 
 // define your styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: MyColors.baseColor,
-  },
-});
+const styles = StyleSheet.create({});
 
 //make this component available to the app
 export default Feed;
