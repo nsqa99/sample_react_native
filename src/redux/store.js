64931af762d9aba921166667a339961eef37597a
NSQA <nguyensyquanganh@gmail.com>
2020-11-26 23:01:27 +0700

    change

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,5 @@
 import counterReducer from "./reducers/counterReducer";
 import { createStore } from "redux";
 
-console.log("here");
-
 const store = createStore(counterReducer);
-console.log(store);
 export default store;
